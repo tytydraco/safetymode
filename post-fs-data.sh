5831ad9ca85dc2427f8848c253e269da0f7f5523
Tyler Nijmeh <tylernij@gmail.com>
2020-08-04 02:55:16 +0000

    Initial commit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ post-fs-data.sh
@@ -0,0 +1,25 @@
+#!/system/bin/sh
+# Written by Draco (tytydraco @ GitHub)
+
+UPCOUNT=0
+DOWNCOUNT=0
+while true
+do
+	if [[ $UPCOUNT -ge 3 ]]
+	then
+		touch "/data/cache/.disable_magisk"
+		reboot
+	fi
+
+	if [[ $DOWNCOUNT -ge 3 ]]
+	then
+		rm "/data/cache/.disable_magisk"
+		reboot
+	fi
+
+	EVENT=`getevent -q -l -c 1`
+	echo "$EVENT" | grep KEY_VOLUMEDOWN | grep DOWN &&
+		DOWNCOUNT=$(($DOWNCOUNT + 1))
+	echo "$EVENT" | grep KEY_VOLUMEUP | grep DOWN &&
+		UPCOUNT=$(($UPCOUNT + 1))
+done
